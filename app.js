d34664f8fa7c4d2759fc09ad25fd45cd2024efbf
Michael Kawata <michaelkawata@bens-air.lan>
2022-12-17 12:08:54 -0800

    adding jump function

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const block = document.getElementById("block");
 const safezone = document.getElementById("safezone");
-const character = document.getElementById("character")
+const character = document.getElementById("character");
+const jumping = 0;
 
 // Randomize safe zones
 safezone.addEventListener('animationiteration', () => {
-    const random = (Math.random() * 3)
-    const top = (random * 100) + 150;
-    safezone.style.top = -(top) + "px";
+    const random = -((Math.random() * 300) + 150);
+    safezone.style.top = random + "px";
 });
 
 // Create Gravity
-setInterval(function () {
+setInterval(() => {
     const topOfCharacter = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    character.style.top = (topOfCharacter + 3) + "px";
+    if (jumping == 0) {
+        character.style.top = (topOfCharacter + 3) + "px";
+    }
 }, 10);
+
+// Create jump function
+const jump = () => {
+    jumping = 1;
+    const jumpCount = 0;
+    const jumpInterval = setInterval(() => {
+        const topOfCharacter = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if ((topOfCharacter > 6) && (jumpCount < 15)) {
+            character.style.top = (topOfCharacter - 5) + "px";
+        }
+        if (jumpCount > 20) {
+            clearInterval(jumpInterval);
+            jumping = 0;
+            jumpCount = 0;
+        }
+        jumpCount++;
+    }, 10)
+}
