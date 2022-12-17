6a06275f10e7991698ac040e3e2c3d757e3ae969
Michael Kawata <michaelkawata@bens-air.lan>
2022-12-17 10:16:37 -0800

    created character and gravity

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const block = document.getElementById("block");
 const safezone = document.getElementById("safezone");
+const character = document.getElementById("character")
 
 // Randomize safe zones
+safezone.addEventListener('animationiteration', () => {
+    const random = (Math.random() * 3)
+    const top = (random * 100) + 150;
+    safezone.style.top = -(top) + "px";
+});
+
+// Create Gravity
+setInterval(function () {
+    const topOfCharacter = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    character.style.top = (topOfCharacter + 3) + "px";
+}, 10);
